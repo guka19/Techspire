574886adcff4d42b7f13730bdc52d52f4e475fc6
guka19 <22100570@ibsu.edu.ge>
2026-02-11 12:19:15 +0400

    123

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://techspire-api.onrender.com',
 });
 
 // This "Interceptor" automatically attaches your Bearer token to every request
